b60a26ad76687ea0ccd2e7a41c18b13eb23e236a
Neslin <neslin_baydar@hotmail.com>
2019-10-06 22:44:22 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+///TERNARY OPERATOR
+const person = 'Perseus';
+2- person === 'Medusa' ? console.log('Yep, Medusa!') : 
+3- console.log(`Nope, not Medusa. That's ${person}.`);
+
+const number="120";
+
+if(number ==100)
+{
+    console.log("sayı 100'e esit")
+}
+else if(number!=100){
+    console.log("sayi 100'e esit degil");
+}
+
+////aynı örneği ternary operatörüyle yap////
+ console.log(number==100 ? "sayi 100' esit":"sayi 100'e esit degil");
